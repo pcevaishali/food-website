68a0d945c66e0ffed2772ac077a971bf56a7a64b
Rohit Sharma <rohitshar0801@gmail.com>
2024-05-14 14:57:15 +0530

    Image-loading-error

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,5 +1,5 @@
 
-export const IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_140,h_140,c_fill/";
+export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 
 
