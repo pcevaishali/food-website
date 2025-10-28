e3cc24bb22172c23c0ae5ff620ce5c4ef12202d3
pcevaishali <2022pcecavaishali059@poornima.org>
2025-10-28 11:45:15 +0530

    Project setup done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,32 @@
-
 import heartt from "../Images/heartt.png";
 export const Footer = () => {
-
-
   return (
     <div className=" flex justify-center align-middle w-auto  text-2xl pt-5 pr-2.5 pb-2 pl-2.5  mb-5 rounded-md shadow-2xl shadow-slate-400">
       <strong className="mx-1">
-
         <b>Foodie</b>
       </strong>
       Created By
-
-
-      <a className=" text-red-600 hover:text-purple-800 text-2xl mx-1 "
-        href="https://www.linkedin.com/in/rohit-sharma50/"
+      <a
+        className=" text-red-600 hover:text-purple-800 text-2xl mx-1 "
+        href="https://www.linkedin.com/in/vaishali-sharma-8b65aa277/"
         target="_blank"
-        title="Rohit Sharma's Linkedin Profile"
+        title="Vaishali Sharma's Linkedin Profile"
       >
-        <b>Rohit Sharma</b></a>
+        <b>Vaishali Sharma</b>
+      </a>
       <div>with </div>
-
-
-
-      <div className="text-2xl p-1 "> <img className='image w-9' src={heartt} alt='heart crossed' ></img> </div>
-      <a className=" text-red-600 hover:text-purple-800 text-2xl mx-1 "
+      <div className="text-2xl p-1 ">
+        {" "}
+        <img className="image w-9" src={heartt} alt="heart crossed"></img>{" "}
+      </div>
+      <a
+        className=" text-red-600 hover:text-purple-800 text-2xl mx-1 "
         href="https://react.dev/"
         target="_blank"
         title="React offical web link"
-      >➡ <b>React</b> </a>
+      >
+        ➡ <b>React</b>{" "}
+      </a>
     </div>
   );
-
 };
-
-
-
-
-
